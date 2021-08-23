8bcd16c5ed8f31e98271436a86b5741256150074
vincent <vincent.arteaga@gmail.com>
2021-08-22 21:37:42 -0700

    created reaact files in front end folder

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { CATEGORIES } from "./data";
+import DisplayTask from "./components/DisplayTask";
 
 function App() {
   return (
